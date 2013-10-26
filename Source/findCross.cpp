b37713b0169c102e64aaa2769abcf69e830cb70c
Yannick Gaudin <ygaudin@insa-rennes.fr>
2013-10-26 19:28:36 +0200

    Full wraping + add infos in console
    Signed-off-by: Yannick Gaudin <ygaudin@insa-rennes.fr>

--- Source/findCross.cpp
+++ Source/findCross.cpp
@@ -16,6 +16,8 @@ using namespace std;
 #include <math.h>
 #define M_PI 3.14159265358979323846264338327
 
+#define ANGLE_REFERENCE (double)54.88302
+
 char name_windows[20];
 
 int match_method = 5; // > 5 est la plus performante > testée jusqu'à +/- 12°
@@ -45,7 +47,7 @@ int main (void) {
 	Mat full_image_usable_down = full_image_resized(roi_middle_down);
 
 	/*********** If you want to add rotation on the source image *******************/
-	double angle = 5 ;
+	double angle = 0 ;
 	rotateE(full_image_usable, angle, full_image_usable);
 	rotateE(full_image_usable_up, angle, full_image_usable_up);
 	rotateE(full_image_usable_down, angle, full_image_usable_down);
@@ -56,17 +58,55 @@ int main (void) {
 	Size size_template( template_cross.cols/reduction_factor, template_cross.rows/reduction_factor );
 	resize( template_cross, template_usable/*********/, size_template );	
 
-
 	/// Create Trackbar
 //	char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
 //	createTrackbar( trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod );
-	Point origine_template;
 
-	MatchingMethod( full_image_usable_up, template_usable, origine_template );
-	printf("Coordonnees 1ere croix :\n x : %d\n y : %d\n", origine_template.x, origine_template.y);
+	Point origine_cross_up;
+	Point origine_cross_down;
 
-	MatchingMethod( full_image_usable_down, template_usable, origine_template );
-	printf("\n\nCoordonnees 2eme croix :\n x : %d\n y : %d\n", origine_template.x, origine_template.y);
+	MatchingMethod( full_image_usable_up, template_usable, origine_cross_up );
+	printf("Coordonnees 1ere croix :\n x : %d\n y : %d\n", origine_cross_up.x, origine_cross_up.y);
+
+	MatchingMethod( full_image_usable_down, template_usable, origine_cross_down );
+	printf("\n\nCoordonnees 2eme croix :\n x : %d\n y : %d\n", origine_cross_down.x, origine_cross_down.y);
+	origine_cross_down.y += full_image_usable.size().height/2 ;
+	printf("\n\nCoordonnees 2eme croix (img complete) :\n x : %d\n y : %d\n\n", origine_cross_down.x, origine_cross_down.y);
+
+
+/*
+*		 origine_cross_up
+*		/|
+*  hyp / |
+* ------------
+*	 /	 | op
+*	/____|
+*	^   adj
+*	origine_cross_down
+*/
+
+	double op = origine_cross_down.y - origine_cross_up.y ;
+	double adj = origine_cross_up.x - origine_cross_down.x ;
+	double hyp = sqrt( pow(op,2) + pow(adj,2) );
+
+	/* Compute the angle */
+	double result_tan, result_cos, result_sin ;
+	result_tan = atan( (double)(op) / (adj) ) ;// tan(angle) = op / adj
+	printf("resultat angle (tan) : %f degres\n", result_tan * 180 / M_PI );
+
+	result_cos = acos( (double)(adj) / (hyp) ) ;
+	printf("resultat angle (cos) : %f degres\n", result_cos * 180 / M_PI );
+
+	result_sin = asin( (double)(op) / (hyp) ) ;
+	printf("resultat angle (sin) : %f degres\n", result_sin * 180 / M_PI );
+
+	double mean_angle = (result_tan + result_cos + result_sin)/3 ;
+	printf("\nangle moyen : %f degres\n", mean_angle * 180 / M_PI );
+	double correction_angle = ANGLE_REFERENCE - (mean_angle * 180 / M_PI) ;
+	printf("inclinaison image : %f degres\n",  correction_angle);
+
+	rotateE(full_image_usable, correction_angle, full_image_usable);
+	imshow( "image droite", full_image_usable );
 
 	//termine le programme lorsqu'une touche est frappee
 	waitKey(0);
