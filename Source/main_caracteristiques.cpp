fba0233320f6d7e72f7b07cf99a3a0240057a4d8
unknown <acharrier@creo.ca>
2013-12-10 15:03:51 +0100

    main_caracteristiques added

---
+++ Source/main_caracteristiques.cpp
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////
+// Module IRF, 5-INFO 5-EII
+// contenu : creer la base de donnée de la reconnaissance d'images
+// auteur : Yannick Gaudin, Martin Seillan, Alexandra Charrier, Richard Lagrange
+// date : 26/10/2013
+//////////////////////////////////////////////////////////////////////////
+
+#include "cv.h"
+#include "highgui.h"
+using namespace cv;
+#include <iostream>
+#include <string>
+using namespace std;
+#include <Windows.h>
+
+int main(void){
+
+	cout << "main\n";
+
+
+	system("pause");
+	waitKey(0);
+	return 0;
+}
